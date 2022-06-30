f2d7098fcb19d3116096cf19f49cac94edb2ac8b
Yujiro Kato <yu-katou@shibuya.co.jp>
2022-06-30 20:15:21 +0900

    描画サンプルを変更。
    LookAt や CreateOrthographic の設定がうまく反映されていない。
    SetProjection() すると描画したはずの正方形が見えないのでとりあえず即時 return している。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
@@ -22,71 +21,60 @@ namespace OpenTkApp1
                 MinorVersion = 1,
             };
             this.tkControl.Start(settings);
+
+            Matrix4 look = Matrix4.LookAt(Vector3.UnitZ, Vector3.Zero, Vector3.UnitY);
+            GL.LoadMatrix(ref look);
+            GL.Enable(EnableCap.DepthTest);
         }
 
         private void tkControl_OnLoaded(object sender, RoutedEventArgs e)
         {
-            //SetProjection();
+            SetProjection();
         }
 
         private void tkControl_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            //SetProjection();
+            SetProjection();
         }
-private static readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+
         private void tkControl_OnRender(TimeSpan delta)
         {
-            var alpha = 1.0f;
-            var hue = (float) _stopwatch.Elapsed.TotalSeconds * 0.15f % 1;
-            var c = Color4.FromHsv(new Vector4(alpha * hue, alpha * 0.75f, alpha * 0.75f, alpha));
-            GL.ClearColor(c);
+            GL.ClearColor(Color4.Black);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.LoadIdentity();
-            GL.Begin(PrimitiveType.Triangles);
 
-            GL.Color4(Color4.Red);
-            GL.Vertex2(0.0f, 0.5f);
+            GL.PushMatrix();
+            {
+                // (-2, -2) まで移動
+                GL.Translate(-2.0, -2.0, 0.0);
 
-            GL.Color4(Color4.Green);
-            GL.Vertex2(0.58f, -0.5f);
+                for (int i = 0; i < 5; i++) // x 方向
+                {
+                    GL.PushMatrix();
+                    {
+                        for (int j = 0; j < 5; j++) // y 方向
+                        {
+                            // 正方形を描画
+                            DrawSquare(new Color4(i * 0.25f, j * 0.25f, 0.0f, 255.0f));
+                            // y 方向に1移動
+                            GL.Translate(0.0, 1.0, 0.0);
+                        }
+                    }
+                    GL.PopMatrix();
 
-            GL.Color4(Color4.Blue);
-            GL.Vertex2(-0.58f, -0.5f);
-
-            GL.End();
-            // GL.PushMatrix();
-            // {
-            //     // (-2, -2) まで移動
-            //     GL.Translate(-2.0, -2.0, 0.0);
-
-            //     for (int i = 0; i < 5; i++) // x 方向
-            //     {
-            //         GL.PushMatrix();
-            //         {
-            //             for (int j = 0; j < 5; j++) // y 方向
-            //             {
-            //                 // 色の変更
-            //                 GL.Color3(i * 0.25, j * 0.25, 0.0);
-            //                 // 正方形を描画
-            //                 DrawSquare();
-            //                 // y 方向に1移動
-            //                 GL.Translate(0.0, 1.0, 0.0);
-            //             }
-            //         }
-            //         GL.PopMatrix();
-
-            //         // x 方向に1移動
-            //         GL.Translate(1.0, 0.0, 0.0);
-            //     }
-            // }
-            // GL.PopMatrix();
+                    // x 方向に1移動
+                    GL.Translate(1.0, 0.0, 0.0);
+                }
+            }
+            GL.PopMatrix();
 
             GL.Finish();
         }
 
         private void DrawSquare()
         {
-            double side = 0.45;
+            const double side = 0.45;
+
             GL.Begin(PrimitiveType.TriangleStrip);
             {
                 GL.Vertex2(side, side);
@@ -97,19 +85,28 @@ private static readonly Stopwatch _stopwatch = Stopwatch.StartNew();
             GL.End();
         }
 
+        private void DrawSquare(Color4 color)
+        {
+            GL.Color4(color);
+            DrawSquare();
+        }
+
         private void SetProjection()
         {
+            return;
+
             // ビューポートの設定
             GL.Viewport(0, 0, (int)this.tkControl.ActualWidth, (int)this.tkControl.ActualHeight);
 
             // 視体積の設定
             GL.MatrixMode(MatrixMode.Projection);
-            var r = this.tkControl.ActualWidth / this.tkControl.ActualHeight;
-            float h = 6.0f, w = (float)(h * r);  //-2 <= y <= 2 に変更し、 (幅) = (高さ) × (アスペクト比) で歪みが出ないように
-            Matrix4 proj = Matrix4.CreateOrthographic(w, h, 0.01f, 2.0f);
-            GL.LoadMatrix(ref proj);
-
-            // MatrixMode を元に戻す
+            {
+                var r = this.tkControl.ActualWidth / this.tkControl.ActualHeight;
+                float h = 6.0f, w = (float)(h * r);  //-3 <= y <= 3 に変更し、 (幅) = (高さ) × (アスペクト比) で歪みが出ないように
+                //Matrix4 proj = Matrix4.CreateOrthographic(w, h, 0.01f, 2.0f);
+                Matrix4 proj = Matrix4.CreateOrthographicOffCenter(-w / 2, w / 2, -h / 2, h / 2, 0.01f, 2.0f);
+                GL.LoadMatrix(ref proj);
+            }
             GL.MatrixMode(MatrixMode.Modelview);
         }
     }
