ca35290a1f4f662e11e67ea4d764d1fa999369c8
hashimoto <yuui-hashimoto@shibuya.co.jp>
2022-08-29 18:17:23 +0900

    生成したテクスチャを格納するコレクションを定義。

--- OpenTkApp1/Views/TKLineGraph.cs
+++ OpenTkApp1/Views/TKLineGraph.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
+using System.Collections.Generic;
 using OpenTK.Wpf;
 using OpenTK;
 
@@ -250,6 +251,13 @@ namespace OpenTkApp1.Views
 
         #endregion 依存関係プロパティ定義
 
+        /// <summary>
+        /// テクスチャのコレクションを生成します。
+        /// </summary>
+        List<int> Textures = new List<int>();
+        TKBitmap LegendBitmap = new TKBitmap();
+        TKBitmap TestBitmap = new TKBitmap();
+
         public void Render()
         {
             // 1回目のRenderが走るタイミングがBindingするより早いため
@@ -275,7 +283,8 @@ namespace OpenTkApp1.Views
                 DrawScale();
                 GL.Translate((XRange / 2), 0, 0);
                 // 凡例描画
-                DrawLegend();
+                DrawLegend(LegendBitmap);
+                DrawTestText(TestBitmap);
             }
             GL.PopMatrix();
            
@@ -544,13 +553,30 @@ namespace OpenTkApp1.Views
         /// <summary>
         /// 凡例を描画するメソッドです。
         /// </summary>
-        private void DrawLegend()
+        private void DrawLegend(TKBitmap bitmap)
         {
             float r = (float)TkGraphics.CurrentWidth / (float)TkGraphics.CurrentHeight;
             float h = 2 * (float)TkGraphics.CurrentHeight;
             float w = h * r;
-            GL.Translate(-_bmpWidth / 2, -h / 2, 0);
-            DrawString();
+            GL.Translate(-bitmap.Width / 2, -h / 2, 0);
+            if (Textures.Count > 0)
+            // 指定したIDのテクスチャを現在のテクスチャとします。
+            GL.BindTexture(TextureTarget.Texture2D, Textures[0]);
+
+            DrawString(bitmap);
+        }
+
+        private void DrawTestText(TKBitmap bitmap)
+        {
+            float r = (float)TkGraphics.CurrentWidth / (float)TkGraphics.CurrentHeight;
+            float h = 2 * (float)TkGraphics.CurrentHeight;
+            float w = h * r;
+            GL.Translate(2 * LegendBitmap.Width , 0, 0);
+            if (Textures.Count > 0)
+                // 指定したIDのテクスチャを現在のテクスチャとします。
+                GL.BindTexture(TextureTarget.Texture2D, Textures[1]);
+
+            DrawString(bitmap);
         }
         /// <summary>
         /// 文字を描画するメソッドです。
@@ -559,27 +585,26 @@ namespace OpenTkApp1.Views
         /// <param name="str"></param>
         /// <param name="fontSize"></param>
         /// <param name="color"></param>
-        public void DrawString()
+        public void DrawString(TKBitmap bitmap)
         {
             // テキストのサイズを画面の大きさに依存しないように領域を定義します。
             SetTextProjection();
 
-            // テクスチャを使用する設定を行います。
-            SettingTexture();
-            
+            // テクスチャを有効化します。
+            GL.Enable(EnableCap.Texture2D);
+
             // 四角形で描画
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0, 0);
             GL.Vertex2(0, 0);
             GL.TexCoord2(1, 0);
-            GL.Vertex2(_bmpWidth, 0);
+            GL.Vertex2(bitmap.Width, 0);
             GL.TexCoord2(1, 1);
-            GL.Vertex2(_bmpWidth, _bmpHeight);
+            GL.Vertex2(bitmap.Width, bitmap.Height);
             GL.TexCoord2(0, 1);
-            GL.Vertex2(0, _bmpHeight);
+            GL.Vertex2(0, bitmap.Height);
             GL.End();
 
-            //if (!_isTexture)
             GL.Disable(EnableCap.Texture2D);
             
             SetGraphProjection();
@@ -593,7 +618,8 @@ namespace OpenTkApp1.Views
         public void CreateTextBitmap()
         {
             if (DrawingItem.Legend == null) return;
-            CreateTextBitmap(DrawingItem.Legend, 32, Colors.White);
+            CreateTextBitmap(DrawingItem.Legend, 32, Colors.White, LegendBitmap) ;
+            CreateTextBitmap("Test", 48, Colors.Orange, TestBitmap);
         }
 
         /// <summary>
@@ -602,7 +628,7 @@ namespace OpenTkApp1.Views
         /// <param name="str"></param>
         /// <param name="fontSize"></param>
         /// <param name="color"></param>
-        public void CreateTextBitmap(string str, double fontSize, Color color)
+        public void CreateTextBitmap(string str, double fontSize, Color color, TKBitmap bitmap)
         {
             var window = Application.Current.MainWindow;
 
@@ -638,23 +664,27 @@ namespace OpenTkApp1.Views
             bmp.Render(drawingVisual);
 
             // ビットマップの幅、高さ取得
-            _bmpWidth = bmp.PixelWidth;
-            _bmpHeight = bmp.PixelHeight;
+            bitmap.Width = bmp.PixelWidth;
+            bitmap.Height = bmp.PixelHeight;
             // stride: 画像の横１列分のデータサイズ = 画像の横幅 * 1画素あたりのbyte(4byte) 
-            int stride = _bmpWidth * 4;
+            int stride = bmp.PixelWidth * 4;
             // ビットマップ全体のサイズ分の配列を定義
-            byte[] tmpbits = new byte[stride * _bmpHeight];
-            var rectangle = new Int32Rect(0, 0, _bmpWidth, _bmpHeight);
+            byte[] tmpbits = new byte[stride * bmp.PixelHeight];
+            var rectangle = new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight);
             bmp.CopyPixels(rectangle, tmpbits, stride, 0);
             // ビットマップを上下反転させる。画像空間の座標と、テクスチャ空間の座標が反転しているため。画像空間は左上原点の軸方向が第4事象、テクスチャ空間は左下原点の第1事象。
-            _bits = new byte[stride * _bmpHeight];
-            for (int h = 0; h < _bmpHeight; h++)
+            _bits = new byte[stride * bmp.PixelHeight];
+            for (int h = 0; h < bmp.PixelHeight; h++)
             {
                 for (int w = 0; w < stride; w++)
                 {
-                    _bits[h * stride + w] = tmpbits[(_bmpHeight - 1 - h) * stride + w];
+                    _bits[h * stride + w] = tmpbits[(bmp.PixelHeight - 1 - h) * stride + w];
                 }
             }
+
+            // 作成したビットマップをテクスチャに貼り付ける設定を行います。
+            SettingTexture(bitmap);
+
         }
         #endregion テキストビットマップ作成
 
@@ -691,33 +721,29 @@ namespace OpenTkApp1.Views
         /// <summary>
         /// テクスチャの設定を行うメソッドです。
         /// </summary>
-        private void SettingTexture()
+        private void SettingTexture(TKBitmap bitmap)
         {
-            //_isTexture = GL.IsEnabled(EnableCap.Texture2D);
             // テクスチャを有効化します。
             GL.Enable(EnableCap.Texture2D);
 
             // テクスチャIDの作成
-            //int texture = GL.GenTexture();
-            // TODO: なぜか↓をコメントアウトすると動作が軽くなった。なぜ？
-            // テクスチャ用バッファの紐づけ                       
-            //GL.BindTexture(TextureTarget.Texture2D, texture);
+            int texture = GL.GenTexture();
+
+            // テクスチャIDをコレクションに追加します。
+            Textures.Add(texture);
+
+            // 指定したIDのテクスチャを現在のテクスチャとします。 
+            GL.BindTexture(TextureTarget.Texture2D, texture);
 
             // テクスチャの拡大・縮小時の補間方法の設定をします。
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
             // ビットマップをテクスチャに割り当てます。
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, _bmpWidth, _bmpHeight, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, _bits);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bitmap.Width, bitmap.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, _bits);
+
         }
 
-        /// <summary>
-        /// 作成したビットマップの横幅を表します。
-        /// </summary>
-        private int _bmpWidth;
-        /// <summary>
-        /// 作成したビットマップの高さを表します。
-        /// </summary>
-        private int _bmpHeight;
         /// <summary>
         /// ビットマップの配列を格納します。
         /// </summary>
